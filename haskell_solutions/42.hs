62f4d207ecbca258ab8115ffd0ddd39c4de1927b
Matt Wolf <mattwollf@gmail.com>
2015-03-21 06:16:09 -0400

    finished 42

--- haskell_solutions/42.hs
+++ haskell_solutions/42.hs
@@ -1,3 +1,21 @@
+import Data.List
+import Data.Char (ord)
+import Data.List.Split
+
 triangles :: [Integer]
 
 triangles = [ (n * n + n) `div` 2 | n <- [1..]]
+
+isTriangle :: Integer -> Bool
+isTriangle n = n `elem` takeWhile (<= n) triangles
+
+letterPosition :: Char -> Integer
+letterPosition c = toInteger $ ord c - 64
+
+isLetter :: Char -> Bool
+isLetter c = and [n > 0,n < 27] where
+	n = ord c - 64
+	
+main = do
+	s <- readFile "p042_words.txt"
+	print $ length $ filter isTriangle $ map sum $ map (map letterPosition) $ map (filter isLetter) $ splitOn "," s
